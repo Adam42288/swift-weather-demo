bd79989e80538b288176b56f624fd10da68d9ea0
Adam <asantillana@apple.com>
2023-01-30 16:24:04 -0600

    Initial Commit

---
+++ swift-weather-demo/swift_weather_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_weather_demoApp.swift
+//  swift-weather-demo
+//
+//  Created by Adam Santillana on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_weather_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
